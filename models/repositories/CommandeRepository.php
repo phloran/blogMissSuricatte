27d8cda200278aa861a0f418c9c47d4c95d524d5
alaixy <alexismortier@msn.com>
2017-01-30 16:17:07 +0100

    repository

---
+++ models/repositories/CommandeRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+class CommandeRepository
+{
+	public function getAll($pdo) {
+		$resultats = $pdo->query('SELECT cp.quantite, p.libelle, c.date_cmd, c.date_expedition FROM commande_produit cp 
+INNER JOIN produit p ON cp.prd_id = p.id
+INNER JOIN commande c ON cp.com_id = c.id');
+
+		$resultats->setFetchMode(PDO::FETCH_OBJ);
+	
+	$listeCommande = array();
+
+		while($obj = $resultats->fetch()){	
+
+			$commande = new Commande();
+			$commande->setQuantite($obj->quantite);
+			$commande->setLibelle($obj->libelle);
+			$commande->setDateCmd($obj->date_cmd);
+			$commande->setDateExp($obj->date_expedition);
+
+			$listeCommande[] = $commande;
+
+		}
+
+		return $listeCommande;
+	}
+	
+	
+}
